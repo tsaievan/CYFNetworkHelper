a8da66c85132d221dea9df19309df49862529dc4
tsaievan <tsaievan@icloud.com>
2019-03-26 19:29:10 +0800

    get带缓存的一些实现

--- CYFNetworkHelper/CYFNetworkHelper/CYFNetworkHelper/CYFNetworkHelper.m
+++ CYFNetworkHelper/CYFNetworkHelper/CYFNetworkHelper/CYFNetworkHelper.m
@@ -9,6 +9,7 @@
 #import "CYFNetworkHelper.h"
 #import <AFNetworking.h>
 #import <AFNetworkActivityIndicatorManager.h>
+#import "CYFNetworkCache.h"
 
 
 #ifdef DEBUG
@@ -24,6 +25,7 @@
 static BOOL _isOpenLog; ///< 是否已开启日志打印
 static NSMutableArray *_allSessionTask;
 static AFHTTPSessionManager *_sessionManager;
+static NSLock *_lock;
 
 + (void)networkStatusWithBlock:(CYFNetworkStatus)networkStatus {
     [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
@@ -77,31 +79,86 @@ static AFHTTPSessionManager *_sessionManager;
 }
 
 + (void)cancelAllRequest {
-    ///< 锁操作
-    @synchronized (self) {
-        ///< 遍历请求task数组, 逐个取消
-        [[self allSessionTask] enumerateObjectsUsingBlock:^(NSURLSessionTask *task, NSUInteger idx, BOOL * _Nonnull stop) {
-            [task cancel];
-        }];
-        ///< 整个数组清空
-        [[self allSessionTask] removeAllObjects];
-    }
+    ///< 锁操作 @synchronized是效率最低的
+    //    @synchronized (self) {
+    ///< 遍历请求task数组, 逐个取消
+    
+    [[self lock] lock];
+    [[self allSessionTask] enumerateObjectsUsingBlock:^(NSURLSessionTask *task, NSUInteger idx, BOOL * _Nonnull stop) {
+        [task cancel];
+    }];
+    ///< 整个数组清空
+    [[self allSessionTask] removeAllObjects];
+    //    }
+    [[self lock] unlock];
 }
 
 + (void)cancelRequestWithURL:(NSString *)URL {
     if (!URL) {
         return;
     }
-    ///< 锁操作
-    @synchronized (self) {
-        [[self allSessionTask] enumerateObjectsUsingBlock:^(NSURLSessionTask *task, NSUInteger idx, BOOL * _Nonnull stop) {
-            if ([task.currentRequest.URL.absoluteString hasPrefix:URL]) {
-                [task cancel];
-                [[self allSessionTask] removeObject:task];
-                *stop = YES;
-            }
-        }];
-    }
+    ///< 锁操作 @synchronized是效率最低的
+    //    @synchronized (self) {
+    [[self lock] lock];
+    [[self allSessionTask] enumerateObjectsUsingBlock:^(NSURLSessionTask *task, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([task.currentRequest.URL.absoluteString hasPrefix:URL]) {
+            [task cancel];
+            [[self allSessionTask] removeObject:task];
+            *stop = YES;
+        }
+    }];
+    [[self lock] unlock];
+    //    }
+}
+
+#pragma mark - GET请求自动缓存
++ (NSURLSessionTask *)GET:(NSString *)URL
+               parameters:(id)parameters
+            responseCache:(CYFHttpRequestCache)responseCache
+                  success:(CYFHttpRequestSuccess)success
+                   failue:(CYFRequestFailed)failue {
+    ///< 首先读取缓存
+    responseCache != nil ? responseCache([CYFNetworkCache httpCacheForURL:URL parameters:parameters]) : nil;
+    NSURLSessionTask *sessionTask = [_sessionManager GET:URL parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        ///< 如果成功
+        if (_isOpenLog) {
+            CYFLog(@"responseObject = %@", responseObject);
+        }
+        ///< 从任务数组中移除网络请求task
+        [[self allSessionTask] removeObject:task];
+        ///< 走成功的回调
+        success ? success(responseObject) : nil;
+        ///< 对数据进行异步缓存
+        responseCache != nil ? [CYFNetworkCache setHttpCache:responseObject URL:URL parameters:parameters] : nil;
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if (_isOpenLog) {
+            CYFLog(@"error = %@", error);
+        }
+        ///< 移除任务
+        [[self allSessionTask] removeObject:task];
+        ///< 回调
+        failue ? failue(error) : nil;
+    }];
+    ///< 添加sessionTask到数组
+    sessionTask ? [[self allSessionTask] addObject:sessionTask] : nil;
+    return sessionTask;
+}
+
+#pragma mark - 初始化AFHTTPSessionManager相关属性
+
++ (void)load {
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+}
+
++ (void)initialize {
+    ///< 初始会话对象
+    _sessionManager = [AFHTTPSessionManager manager];
+    ///< 设置超时时长为30秒
+    _sessionManager.requestSerializer.timeoutInterval = 30.f;
+    _sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", nil];
+    [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
 }
 
 /**
@@ -112,6 +169,13 @@ static AFHTTPSessionManager *_sessionManager;
         _allSessionTask = [[NSMutableArray alloc] init];
     }
     return _allSessionTask;
+}
+
++ (NSLock *)lock {
+    if (!_lock) {
+        _lock = [[NSLock alloc] init];
+    }
+    return _lock;
 }
 
 @end
